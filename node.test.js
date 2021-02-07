b3bb9a1b1e54d22967b786817a0598d75f39c672
nin-jin <nin-jin@users.noreply.github.com>
2021-02-07 07:17:30 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -5971,7 +5971,10 @@ var $;
                 return decodeURIComponent(this.uri().split(this.host(), 2)[1]);
             }
             sub() {
-                return [this.Icon(), ...this.content()];
+                return [
+                    ...this.host() ? [this.Icon()] : [],
+                    ...this.content(),
+                ];
             }
         }
         __decorate([
@@ -6015,6 +6018,7 @@ var $;
         }
         Link(id) {
             const obj = new this.$.$mol_text_link();
+            obj.target = () => this.link_target(id);
             return obj;
         }
         Image(id) {
@@ -6056,6 +6060,9 @@ var $;
         }
         block_type(id) {
             return "";
+        }
+        link_target(id) {
+            return "_blank";
         }
         header_level(id) {
             return 0;
@@ -6658,7 +6665,7 @@ var $;
                 flex: {
                     grow: 1000,
                     shrink: 1,
-                    basis: 'auto',
+                    basis: per(50),
                 },
             },
             Tools: {
@@ -6991,6 +6998,18 @@ var $;
 var $;
 (function ($) {
     var _a;
+    const TextDecoder = (_a = globalThis.TextDecoder) !== null && _a !== void 0 ? _a : $node.util.TextDecoder;
+    function $mol_charset_decode(value, code = 'utf8') {
+        return new TextDecoder(code).decode(value);
+    }
+    $.$mol_charset_decode = $mol_charset_decode;
+})($ || ($ = {}));
+//decode.js.map
+;
+"use strict";
+var $;
+(function ($) {
+    var _a;
     const TextEncoder = (_a = globalThis.TextEncoder) !== null && _a !== void 0 ? _a : $node.util.TextEncoder;
     const encoder = new TextEncoder();
     function $mol_charset_encode(value) {
@@ -6999,18 +7018,6 @@ var $;
     $.$mol_charset_encode = $mol_charset_encode;
 })($ || ($ = {}));
 //encode.js.map
-;
-"use strict";
-var $;
-(function ($) {
-    var _a;
-    const TextDecoder = (_a = globalThis.TextDecoder) !== null && _a !== void 0 ? _a : $node.util.TextDecoder;
-    function $mol_charset_decode(value, code = 'utf8') {
-        return new TextDecoder(code).decode(value);
-    }
-    $.$mol_charset_decode = $mol_charset_decode;
-})($ || ($ = {}));
-//decode.js.map
 ;
 "use strict";
 var $;
@@ -7084,8 +7091,14 @@ var $;
             return match ? match[1].substring(1) : '';
         }
         text(next, force) {
-            const buffer = next === undefined ? undefined : $.$mol_charset_encode(next);
-            return $.$mol_charset_decode(this.buffer(buffer, force));
+            if (next === undefined) {
+                return $.$mol_charset_decode(this.buffer(undefined, force));
+            }
+            else {
+                const buffer = next === undefined ? undefined : $.$mol_charset_encode(next);
+                this.buffer(buffer, force);
+                return next;
+            }
         }
         fail(error) {
             this.buffer(error, $.$mol_mem_force_fail);
@@ -7133,9 +7146,6 @@ var $;
     __decorate([
         $.$mol_mem
     ], $mol_file.prototype, "exists", null);
-    __decorate([
-        $.$mol_mem
-    ], $mol_file.prototype, "text", null);
     __decorate([
         $.$mol_mem_key
     ], $mol_file, "absolute", null);
@@ -11684,19 +11694,6 @@ var $;
 var $;
 (function ($) {
     $.$mol_test({
-        'encode utf8 string'() {
-            const str = 'Hello, ΧΨΩЫ';
-            const encoded = new Uint8Array([72, 101, 108, 108, 111, 44, 32, 206, 167, 206, 168, 206, 169, 208, 171]);
-            $.$mol_assert_like($.$mol_charset_encode(str), encoded);
-        },
-    });
-})($ || ($ = {}));
-//encode.test.js.map
-;
-"use strict";
-var $;
-(function ($) {
-    $.$mol_test({
         'decode utf8 string'() {
             const str = 'Hello, ΧΨΩЫ';
             const encoded = new Uint8Array([72, 101, 108, 108, 111, 44, 32, 206, 167, 206, 168, 206, 169, 208, 171]);
@@ -11710,6 +11707,19 @@ var $;
     });
 })($ || ($ = {}));
 //decode.test.js.map
+;
+"use strict";
+var $;
+(function ($) {
+    $.$mol_test({
+        'encode utf8 string'() {
+            const str = 'Hello, ΧΨΩЫ';
+            const encoded = new Uint8Array([72, 101, 108, 108, 111, 44, 32, 206, 167, 206, 168, 206, 169, 208, 171]);
+            $.$mol_assert_like($.$mol_charset_encode(str), encoded);
+        },
+    });
+})($ || ($ = {}));
+//encode.test.js.map
 ;
 "use strict";
 var $;
