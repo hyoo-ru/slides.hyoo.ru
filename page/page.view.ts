20e6f05053c18618051b0401ddda8c046d49efdf
jin <jin@hyoo.ru>
2021-02-07 21:29:10 +0800

    $mol_boor -> $mol_book2

--- page/page.view.ts
+++ page/page.view.ts
@@ -2,13 +2,15 @@ namespace $.$$ {
 	
 	export class $hyoo_slides_page extends $.$hyoo_slides_page {
 		
-		listener_width() {
-			if( this.role() === 'listener' ) return Number.POSITIVE_INFINITY
-			return super.listener_width()
-		}
-
 		uri_page() {
 			return this.uri_base() + `#slide=${ this.slide() }`
+		}
+		
+		pages() {
+			return [
+				... this.role() === 'listener' ? [] : [ this.Speaker() ],
+				this.Listener(),
+			]
 		}
 		
 	}
