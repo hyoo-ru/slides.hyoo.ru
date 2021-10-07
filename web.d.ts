424d361c5889c58bd20429aaff3da265cdfb15e9
nin-jin <nin-jin@users.noreply.github.com>
2021-10-07 07:20:37 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.d.ts
+++ web.d.ts
@@ -1320,6 +1320,7 @@ declare namespace $ {
     class $mol_button extends $mol_view {
         enabled(): boolean;
         minimal_height(): number;
+        minimal_width(): number;
         click(event?: any): any;
         event_click(event?: any): any;
         event(): {
