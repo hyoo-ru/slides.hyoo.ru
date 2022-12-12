89e726fd2b4bd69d5642c2cdab657862dd0db09b
nin-jin <nin-jin@users.noreply.github.com>
2022-12-12 07:18:59 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.test.js
+++ web.test.js
@@ -2727,6 +2727,27 @@ var $;
 ;
 "use strict";
 var $;
+(function ($_1) {
+    $mol_test_mocks.push($ => {
+        class $mol_locale_mock extends $mol_locale {
+            lang(next = 'en') { return next; }
+            static source(lang) {
+                return {};
+            }
+        }
+        __decorate([
+            $mol_mem
+        ], $mol_locale_mock.prototype, "lang", null);
+        __decorate([
+            $mol_mem_key
+        ], $mol_locale_mock, "source", null);
+        $.$mol_locale = $mol_locale_mock;
+    });
+})($ || ($ = {}));
+//mol/locale/locale.test.ts
+;
+"use strict";
+var $;
 (function ($) {
     $mol_test({
         'null by default'() {
