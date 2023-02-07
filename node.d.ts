786ffa6d8e3dfb0b2717e88fed0c4d7f7a5468be
nin-jin <nin-jin@users.noreply.github.com>
2023-02-07 07:17:46 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.d.ts
+++ node.d.ts
@@ -1162,6 +1162,7 @@ declare namespace $ {
         line_height(): number;
         letter_width(): number;
         width_limit(): number;
+        row_width(): number;
         sub(): readonly any[];
     }
 }
@@ -2214,7 +2215,8 @@ declare namespace $ {
         auto(): readonly any[];
         Paragraph(id: any): $$.$mol_paragraph;
         Quote(id: any): $$.$mol_text;
-        List(id: any): $$.$mol_text;
+        List(id: any): $mol_text_list;
+        item_index(id: any): number;
         Header(id: any): $$.$mol_text_header;
         Pre(id: any): $$.$mol_text_code;
         Cut(id: any): $mol_view;
@@ -2235,6 +2237,7 @@ declare namespace $ {
         uri_resolve(id: any): string;
         quote_text(id: any): string;
         highlight(): string;
+        list_type(id: any): string;
         list_text(id: any): string;
         header_level(id: any): number;
         header_arg(id: any): {};
@@ -2288,6 +2291,8 @@ declare namespace $.$$ {
         header_arg(index: number): {
             [x: string]: string;
         };
+        list_type(index: number): string;
+        item_index(index: number): number;
         pre_text(index: number): string;
         quote_text(index: number): string;
         list_text(index: number): string;
@@ -2380,6 +2385,26 @@ declare namespace $.$$ {
     class $mol_portion extends $.$mol_portion {
         indicator_width_style(): string;
     }
+}
+
+declare namespace $ {
+    class $mol_text_list extends $mol_text {
+        auto_scroll(): any;
+        attr(): {
+            mol_text_list_type: string;
+        };
+        Paragraph(id: any): $mol_text_list_item;
+        type(): string;
+    }
+    class $mol_text_list_item extends $mol_paragraph {
+        attr(): {
+            mol_text_list_item_index: number;
+        };
+        index(): number;
+    }
+}
+
+declare namespace $ {
 }
 
 declare namespace $ {
