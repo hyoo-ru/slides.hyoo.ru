fec3ae407c46ca1dd8d810322bade2dceb53d16c
nin-jin <nin-jin@users.noreply.github.com>
2023-03-27 07:18:21 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.d.ts
+++ web.d.ts
@@ -1666,10 +1666,8 @@ declare namespace $ {
 
 declare namespace $ {
     class $mol_button_copy extends $mol_button_minor {
-        data(): {
-            "text/plain": Blob;
-            "text/html": Blob;
-        };
+        blobs(): readonly Blob[];
+        data(): {};
         sub(): readonly any[];
         text(): string;
         text_blob(next?: any): Blob;
@@ -1686,6 +1684,9 @@ declare namespace $ {
 
 declare namespace $.$$ {
     class $mol_button_copy extends $.$mol_button_copy {
+        data(): {
+            [k: string]: Blob;
+        };
         html(): string;
         attachments(): ClipboardItem[];
         click(event?: Event): void;
