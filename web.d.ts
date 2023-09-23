f9a2f974a0dd8e77af07d4e29303ba5d039f942b
nin-jin <nin-jin@users.noreply.github.com>
2023-09-23 07:14:31 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.d.ts
+++ web.d.ts
@@ -2729,6 +2729,7 @@ declare namespace $ {
         every<Context = null>(check: (this: Context, val: Item, index: number, list: Item[]) => boolean, context?: Context): boolean;
         reverse(): never;
         sort(): never;
+        [Symbol.toPrimitive](): string;
     }
 }
 
