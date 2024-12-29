9b5418bfe658858cb6ba9342393eb37df6ce0717
nin-jin <nin-jin@users.noreply.github.com>
2024-12-29 07:18:50 +0000

    Deploying to gh-pages from @ hyoo-ru/slides.hyoo.ru@1c9eacd1cda58df0820d27939475196665730dfc 🚀

--- node.mjs
+++ node.mjs
@@ -7211,7 +7211,7 @@ var $;
             Tools: {
                 flex: {
                     basis: 'auto',
-                    grow: 1000,
+                    grow: 0,
                     shrink: 1,
                 },
                 display: 'flex',
--- node.test.js
+++ node.test.js
@@ -7202,7 +7202,7 @@ var $;
             Tools: {
                 flex: {
                     basis: 'auto',
-                    grow: 1000,
+                    grow: 0,
                     shrink: 1,
                 },
                 display: 'flex',
