c95f5d20908286614db1d0907733e4012df80f07
nin-jin <nin-jin@users.noreply.github.com>
2026-03-17 07:57:07 +0000

    Deploying to gh-pages from @ hyoo-ru/slides.hyoo.ru@f022d9406f31b2c29d5fe362e883ac9c55c14427 🚀

--- web.mjs
+++ web.mjs
@@ -3864,6 +3864,8 @@ var $;
                 }
                 catch (error) {
                     $mol_fail_log(error);
+                    if (error instanceof Error)
+                        return '💥' + error.message;
                     return '';
                 }
             }
